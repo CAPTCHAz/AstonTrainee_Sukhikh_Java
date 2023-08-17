8b45c5df70fd095e857489dce2bc0e5e248d1ede
CAPTCHAz <Carlyk66@gmail.com>
2023-08-17 15:15:36 +0400

    17.08

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try(Scanner scan = new Scanner(System.in)) {
+            Hello(scan);
+            nameCheck(scan);
+            arrayCheck(scan);
+        }
+    }
+
+    private static void Hello(Scanner scan) {
+        System.out.print("Введите число: ");
+        double num = scan.nextInt();
+        if (num > 7) {
+            System.out.println("Привет");
+        }
+    }
+
+    public static void nameCheck(Scanner scan) {
+        System.out.print("Введите имя: ");
+        String name = scan.next();
+        if (name.equals("Вячеслав")) {
+            System.out.println("Привет, Вячеслав");
+        } else System.out.println("Нет такого имени");
+    }
+
+    private static void arrayCheck(Scanner scan) {
+        System.out.print("Введите длинну массива: ");
+        int size = scan.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Введите элементы массива: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.print("Элементы массива кратные 3: ");
+        for (int j : arr)
+            if (j % 3 == 0) {
+                System.out.print(j + " ");
+            }
+    }
+}
